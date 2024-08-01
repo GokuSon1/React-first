a09506f9d401c873d81df2d25675ef410ef113d5
Muhammad abrar <abrarnkkhan@gmail.com>
2024-08-02 04:32:05 +0500

    Prepare for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
+// next.config.js
+module.exports = {
+    output: 'export',
+    // other Next.js configurations if any
+  }
+  
 export default nextConfig;
